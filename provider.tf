497ea31a11fb9c540777bb82ad8755c81583997f
yaseenkadlemakki <95896816+yaseenkadlemakki@users.noreply.github.com>
2022-01-27 19:06:39 -0500

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+   region = "us-east-2"
+   access_key = "AKIATOJTD6CQORLG4O2W"
+   secret_key = "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"
+ }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+main_vpc_cidr = "10.0.0.0/24"
+ public_subnets = "10.0.0.128/26"
+ private_subnets = "10.0.0.192/26"
+ region = "us-east-2"
